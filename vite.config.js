db6c4e37fcf74a5d5958077008025016c6e7d083
thushanmm <thushan_madhushara@cypsolabs.com>
2025-02-20 00:14:29 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Add this line to set the base path for GitHub Pages
+
 })
