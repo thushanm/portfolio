d6ee1281d73f276d6f7acaf81812cd408bdff2ce
thushanm <thushanmadhushara2@gmail.com>
2025-02-23 23:42:34 +0530

    Save changes before switching branches

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-
-})
+  assetsInclude: ['**/*.woff', '**/*.ttf'],
+  base: '/portfolio/', // GitHub repo name
+});
