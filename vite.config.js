0e8e5a00ca1b745e3f68f7424485d28f95c0191d
thushanm <thushanmadhushara2@gmail.com>
2025-02-24 20:43:14 +0530

    Saving changes before revert

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.woff', '**/*.ttf'],
-  base: '/portfolio/', // GitHub repo name
-});
